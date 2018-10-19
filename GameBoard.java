479810835e0aa320f05166c5ffee6ca6d5aa0aa2
Kristen Bresser <kbressler@carroll.edu>
2018-10-19 15:45:59 -0600

    Worked on GameSquare and GameBoard

--- GameBoard.java
+++ GameBoard.java
@@ -1,4 +1,114 @@
+import java.util.Random;
 
 public class GameBoard {
+	
+	private GameSquare [] startingBoard;
+	private GameSquare [] playingBoard;
+	private GameSquare [] winningBoard;
+	
+	private int boardSize;
+	private int boardSideLength = (int) Math.sqrt(boardSize);
+	private int difficulty;
+	
+	private boolean wonGame;
+	
+	public GameBoard(int boardSize, int difficulty)
+	{
+		this.boardSize = boardSize;
+		this.difficulty = difficulty;
+		
+		createWinningBoard();
+		createStartingBoard();
+		createPlayingBoard();
+		
+	}
+	
+	private void createWinningBoard()
+	{
+		for(int i = 0; i < boardSize; i++)
+		{
+			int currentGameSquareNumber = i + 1;
+			//GameSquare temp = new GameSquare(false, false, Image, Image, upperLeft, currentGameSquraeNumber);
+			//winningBoard[i] = temp;
+			
+		}
+	}
+	
+	private void createStartingBoard()
+	{
+		startingBoard = winningBoard;
+		randomizeBoard();
+	}
+	
+	private void createPlayingBoard()
+	{
+		playingBoard = startingBoard;
+	}
+	
+	
+	
+	private void randomizeBoard()
+	{
+		int DECIDEHOWMANYSPINSTHEYGET = 10;
+		
+		Random random = new Random();
+		for(int i = 0; i < DECIDEHOWMANYSPINSTHEYGET; i++)
+		{
+			int spinLength = random.nextInt(boardSideLength) + 1;
+			int spinHeight = random.nextInt(boardSideLength) + 1;
+			int spinXPosition = random.nextInt(boardSideLength - spinLength);
+			int spinYposition = random.nextInt(boardSideLength - spinHeight);
+			
+			int numOfTilesToSpin = spinLength * spinHeight;
+			
+			GameSquare [] temp = new GameSquare [numOfTilesToSpin];
+			
+			for(int j = 0; j < numOfTilesToSpin; j++)
+			{
+				// NO!!! temp[j] = playingBoard[spinLength*spinHeight]
+			}
+			
+			spin(temp, spinLength, spinHeight);
+		}
+	}
+	
+	private void boardRestrictions()
+	{
+		
+	}
+	
+	private GameSquare [] spin(GameSquare [] toMove, int moveLength, int moveHeight)
+	{
+		int toMoveArraySize = toMove.length;
+		
+		GameSquare [] moved = toMove;
+		
+		for(int i = 0; i < toMoveArraySize; i ++)
+		{
+			moved[i] = toMove[toMoveArraySize - i - 1];
+			moved[i].flipGameSquare();
+		}
+		
+		return moved;
+	}
+	
+	public boolean hasWon()
+	{
+		wonGame = true;
+		for(int i = 0; i < boardSize; i ++)
+		{
+			if(winningBoard[i].getGameSquareNumber() != playingBoard[i].getGameSquareNumber())
+				wonGame = false;
+			else if(playingBoard[i].isUpsideDown())
+				wonGame = false;
+
+		}
+		return wonGame;
+	}
+	
+	public void resetGame()
+	{
+		playingBoard = startingBoard;
+	}
 
 }
--- GameSquare.java
+++ GameSquare.java
@@ -1,4 +1,62 @@
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 
 public class GameSquare {
+	
+	private boolean selected;
+	private boolean upsideDown;
+	private ImageIcon rightSideUpImage;
+	private ImageIcon upsideDownImage;
+	private JLabel gameSquareJLabel;
+	private int[] upperLeft;
+	private int gameSquareNumber;
+	
+	public GameSquare(boolean selected, boolean upsideDown, ImageIcon rightSideUpImage, ImageIcon upsideDownImage, int[] upperLeft, int gameSquareNumber)
+	{
+		this.selected = selected;
+		this.upsideDown = upsideDown;
+		this.upsideDownImage = upsideDownImage;
+		this.rightSideUpImage = rightSideUpImage;
+		this.upperLeft = upperLeft;
+		this.gameSquareNumber = gameSquareNumber;
+	}
+	
+	private boolean isSelected()
+	{
+		return this.selected;
+	}
+	
+	private void setSelected(boolean selected)
+	{
+		this.selected = selected;
+	}
+	
+	public boolean isUpsideDown()
+	{
+		return this.upsideDown;
+	}
+	
+	public void flipGameSquare()
+	{
+		if(!isUpsideDown())
+			this.upsideDown = true;
+		else
+			this.upsideDown = false;
+	}
+	
+	public int getGameSquareNumber()
+	{
+		return this.gameSquareNumber;
+	}
+	
+	public void drawGameSquare()
+	{
+		if(!isUpsideDown())
+			gameSquareJLabel.setIcon(rightSideUpImage);
+		else
+			gameSquareJLabel.setIcon(upsideDownImage);
+	}
 
 }
